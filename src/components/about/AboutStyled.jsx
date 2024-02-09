7e88fe06a13dd59c723f0c00e3cb61fdf31b3c5f
lucasmoraist <luksmn1101@gmail.com>
2024-02-09 16:43:03 -0300

    feat: criando component sobre mim

---
+++ src/components/about/AboutStyled.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const AboutStyled = styled.div`
+    
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+        
+
+    p{
+        font-family: var(--poppins);
+        width: 670px;
+        font-size: 18;
+        color: var(--secondary-text);
+        text-align: justify;
+    }
+`
+
+export default AboutStyled;
