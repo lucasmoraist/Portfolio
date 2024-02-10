ccd3142b6f56c0b45293ff7c97c142e02fcc610b
lucasmoraist <luksmnt1101@gmail.com>
2024-02-10 19:48:36 -0300

    feat: adicionando components no app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./components/header/Header";
 import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
 
 const App = () =>{
     return<>
         <Header/>
         <About/>
+        <Experience/>
+        <Footer/>
     </>
 }
 
