179e8238ded0b4c9b165739c3c0bb78bdce00609
lucasmoraist <luksmnt1101@gmail.com>
2024-02-14 20:51:23 -0300

    feat: adicionando component dos projetos

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from "./components/header/Header";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Footer from "./components/footer/Footer";
+import Project from "./components/projects/Project";
 
 const App = () =>{
     return<>
         <Header/>
         <About/>
         <Experience/>
+        <Project/>
         <Footer/>
     </>
 }
