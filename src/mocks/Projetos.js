158a98bbe1f0599332fce4392b797b4195dd1897
lucasmoraist <luksmn1101@gmail.com>
2024-02-15 07:59:23 -0300

    feat: corrigindo textos

--- src/mocks/Projetos.js
+++ src/mocks/Projetos.js
@@ -14,21 +14,21 @@ const Projetos = [
     {
         id: 2,
         nome: "Gráfico com Chart.js",
-        descricao: "Este projeto foi um desafio proposto pelo cubo do itau, e a ideia era criar uma tabela e gráfico dinâmico, onde o gráfico iria pegar as informações da tabela e ia deveolver em forma de gráfico as informações.",
+        descricao: "Este projeto foi um desafio proposto pelo cubo do itau, e a ideia era criar uma tabela e gráfico dinâmico, onde o gráfico iria pegar as informações da tabela e ia devolver em forma de gráfico as informações.",
         img: grafico,
         link: "https://github.com/lucasmoraist/Participation-Graph"
     },
     {
         id: 3,
         nome: "Security",
-        descricao: "API desenvolvida para aplicação de práticas de segurança. nesta api foi utilizada o spring security junto ao jwt para fazer validação através do token retornado pela resposta do email e senha correto.",
+        descricao: "API desenvolvida para aplicação de práticas de segurança. Nesta api foi utilizada o spring security junto ao jwt para fazer validação através do token retornado pela resposta do email e senha correto.",
         img: security,
         link: "https://github.com/lucasmoraist/Security"
     },
     {
         id: 4,
         nome: "Gerador de Certificado",
-        descricao: "Api Rest desenvolvida para apresentar o resultado do exame prestato pelo aluno. também rankeando eles através de query na interface do projeto.",
+        descricao: "API Rest desenvolvida, junto a Rockseat no NLW, para apresentar o resultado do exame prestato pelo aluno. Também rankeando eles através de query na interface do projeto.",
         img: certificate,
         link: "https://github.com/lucasmoraist/certificate-generator"
     }
