1c5e7f020f0135cfe064fa5ab57734a655e159eb
lucasmoraist <luksmn1101@gmail.com>
2024-02-28 13:33:23 -0300

    feat: criando ação do botão para fazer download

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,6 +4,27 @@ import Nav from "../nav/Nav";
 import Me from "../icon/Me";
 
 const Header = () => {
+
+    const download = async () => {
+        const get = await fetch("https://portfolio-web-lucas.s3.amazonaws.com/Curr%C3%ADculo.pdf")
+            .then(response => response.blob())
+            .then(blob => {
+                
+                var downloadUrl = URL.createObjectURL(blob);
+                var ancora = document.createElement("a");
+
+                ancora.href=downloadUrl;
+                ancora.download='Currículo.pdf';
+                
+                document.body.appendChild(ancora);
+                ancora.click();
+
+                document.body.removeChild(ancora);
+                URL.revokeObjectURL(downloadUrl)
+            })
+        return get;
+    }
+
     return <HeaderStyled>
         {/* Barra de navegação */}
         <Nav />
@@ -14,7 +35,7 @@ const Header = () => {
 
             <h1>Software <span>Developer</span></h1>
             <div>
-                <button>Download CV</button>
+                <button onClick={download}>Download CV</button>
             </div>
         </div>
     </HeaderStyled>
