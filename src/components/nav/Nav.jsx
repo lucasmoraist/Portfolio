28579ff3518db6946d964b3b41bd819f4be4dc8a
lucasmoraist <luksmn1101@gmail.com>
2024-02-28 16:28:59 -0300

    feat: deixando barra de navegação responsiva e com menu hamburguer

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import NavStyled from "./NavStyled";
 
 const Nav = () => {
-    return <NavStyled>
-        <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#experience">Experience</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-        </ul>
+
+    const [burguerClass, setBurguerClass] = useState('burguer-bar unclicked')
+    const [menuClass, setMenuClass] = useState('menu hidden')
+    const [isVisible, setIsVisible] = useState(false);
+
+    const updateMenu = () => {
+        if (!isVisible) {
+            setBurguerClass('burguer-bar clicked')
+            setMenuClass('menu visible')
+        } else {
+            setBurguerClass('burguer-bar unclicked')
+            setMenuClass('menu hidden')
+        }
+
+        setIsVisible(!isVisible)
+    }
+
+    return <NavStyled style={{ width: '100%' }}>
+        <nav>
+            <div className="burguer-menu" onClick={updateMenu}>
+                <div className={burguerClass} ></div>
+                <div className={burguerClass} ></div>
+                <div className={burguerClass} ></div>
+            </div>
+        </nav>
+
+        <div className={menuClass}>
+            <ul >
+                <li><a href="#home">Home</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+
     </NavStyled>
 }
 
